c2a498e7239c777b4c23af52c48c764188aed01b
CDAM08 <cedam08@MacBook-Pro-de-CDAM08.local>
2019-09-10 18:02:27 -0500

    xxx

---
+++ segment_controll/segment_controll/File.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  segment_controll
+//
+//  Created by CDAM08 on 9/10/19.
+//  Copyright © 2019 Klom Company. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Figuras{
+    let nombre: String
+    let image: UIImage
+    let descripcion: String
+    
+    init(nombre:String, image: UIImage, descripcion:String){
+        self.nombre = nombre
+        self.image =  image
+        self.descripcion = descripcion
+    }
+
+    
+    
+}
