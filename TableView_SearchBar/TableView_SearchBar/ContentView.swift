27d3d60bf469b6386cc604f1c2fed5198aef94fd
lsu <CDAM03@MacBook-Pro-de-CDAM03.local>
2019-09-24 17:52:35 -0500

    cafeteria

---
+++ TableView_SearchBar/TableView_SearchBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TableView_SearchBar
+//
+//  Created by CEDAM03 on 9/24/19.
+//  Copyright © 2019 Klom. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
