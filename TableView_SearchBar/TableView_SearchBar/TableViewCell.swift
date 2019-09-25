dde5940ec64c8b96666a83511e1b3b21e6ab02d7
yo <yo>
2019-09-25 10:48:18 -0500

    chatbot

---
+++ TableView_SearchBar/TableView_SearchBar/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  TableView_SearchBar
+//
+//  Created by CEDAM11 on 25/09/19.
+//  Copyright © 2019 Klom. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
