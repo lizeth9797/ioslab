4a421ab212bc107ca0e3ea171e13ca0656247e19
lizeth9797 <karina.ortiz@avao.mx>
2019-10-27 13:13:40 -0600

    proyecto chatbot

--- 13_QuestionBot/QuestionBot/QuestionBot/QuestionAnswerer.swift
+++ 13_QuestionBot/QuestionBot/QuestionBot/QuestionAnswerer.swift
@@ -196,7 +196,6 @@ struct MyQuestionAnswerer {
             return "es hora de comer una cangre burger"
         }
         
-
         // Leif Erikson
           if question.hasSuffix("cuando es el día de leif erikson"){
             return "miércoles 9 de octubre"
@@ -207,19 +206,80 @@ struct MyQuestionAnswerer {
         }
 
         // Caja secreta
-        if question.hasSuffix("que hay en la caja secreta de patricio"){
-            return "miércoles 9 de octubre"
-        }else if question.hasSuffix("qué hay en la caja secreta de patricio?"){
-            return "miércoles 9 de octubre"
-        }else if question.hasSuffix("qué hay en la caja secreta de patricio"){
-            return "miércoles 9 de octubre"
+        if question.hasSuffix("que hay en la caja secreta de patricio?"){
+            return "una foto embarazosa mia en la fiesta de Navidad"
         }
         
-        if question.hasSuffix("patricio"){
-            return " miércoles 9 de octubre   miércoles 9 de octubre   miércoles 9 de octubre   miércoles 9 de octubremiércoles 9 de octubre \n fffff"}
-    
+        // BURBUJA
+        if question.hasSuffix("como hacer una burbuja?"){
+            return "1. das muchas vueltas\n 2.PARAS!\n 3. luego giras la cabeza 3 veces, 1,2,3!\n 4. y.. A NADAR!! Whoo~ Whooaaau~\n 5. luego te paras sobre el pie derecho, NO LO OLVIDES!\n 6. ahora debes girar sobre ti mismo, una y otra vez!\n 7. luego haz esto,y esto y esto y esto y esto y estoy esto y esto\n 8. yyyyyyyy asi es como se hace burbujas"
+        }
+
+        // PADRES
+        if question.hasSuffix("como se llaman tus padres?"){
+            return "Sr. y Sra. Pantalones Cuadrados"
+        }
 
 
+        // VECINOS
+        if question.hasSuffix("como se llaman tus vecinos?"){
+            return "Patricio Estrella y Calamardo Tentáculos"
+        }
+
+        // HIJA DE DON CANGREJO
+        if question.hasSuffix("como se llama la hija de don cangrejo?"){
+            return "Perlita"
+        }
+
+        // INSTRUMENTO DE CALAMARDO
+        if question.hasSuffix("que instrumento toca calamardo?"){
+            return "El clarinete"
+        }
+
+        // MASCOTA DE PATRICIO- ROCKY
+        if question.hasSuffix("como se llama la mascota de patricio?"){
+            return "Rocky y es muy veloz"
+        }
+
+        // ENEMIGO DE DON CANGREJO
+        if question.hasSuffix("como se llama el enemigo de don cangrejo?"){
+            return "Plankton"
+        }
+
+        // RESTAURANTE DE PLANKTON
+        if question.hasSuffix("como se llama el restaurante de plankton?"){
+            return "Balde de Carnada"
+        }
+
+        // SUPERHÉROES
+        if question.hasSuffix("como se llaman tus superheroes favoritos?"){
+            return "Sirenoman y Chico Percebe"
+        }
+
+        // ARENITA
+        if question.hasSuffix("que animal es arenita?"){
+            return "Es una ardilla"
+        }
+
+        // ORIGEN ARENITA
+        if question.hasSuffix("de donde viene arenita?"){
+            return "de la tonta Texas"
+        }
+
+        // MAESTRA
+        if question.hasSuffix("como se llama tu maestra?"){
+            return "Señora Puff"
+        }
+
+        // ESCUELA
+        if question.hasSuffix("donde estudias?"){
+            return "en la Escuela de Navegación de Botes"
+        }
+
+        // CREADORES
+        if question.hasSuffix("quien te creo?"){
+            return "Luis y Alondra"
+        }
 
         return "No te entendi, ¿podrías escribir con minúsculas?"
     
